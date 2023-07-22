aed6f4aefa70c3c2e5d28527e945fbc097e0eab0
Carol Schluter <cfmiller176@gmail.com>
2023-07-22 11:18:28 -0500

    about contact and home links added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import { useState } from 'react';
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from './components/TodoList';
+import NavBar from './components/NavBar';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom"
+
+
 
 function App() {
-  
 
   return (
     <div className = "to-do-app">
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </div>
       <TodoList />
+      
     </div>
      
     );
 }
 
-export default App
+export default App;
